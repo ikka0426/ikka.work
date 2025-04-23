ecb5585dc44bc65158c63975a3264b4d9d828f6b
ikka0426 <ikka0426@gmail.com>
2025-04-23 23:53:47 +0800

    解决本地图片访问不到的问题

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <div>
         <p className="block text-muted-foreground my-2">其实首页什么都没有，可以点开左上角的侧边栏看看有没有你感兴趣的东西。</p>
         <p className="block text-muted-foreground my-2">或者你也可以看看这个：</p>
-        <Image src="/asahi-shakunetsu.png" alt="芹泽朝日和灼热.png" width={512} height={512} className="m-auto"/>
+        <Image src="https://raw.githubusercontent.com/ikka0426/ikka.work/refs/heads/master/public/asahi-shakunetsu.png" alt="芹泽朝日和灼热.png" width={512} height={512} className="m-auto"/>
         <p className="block text-muted-foreground my-2 text-sm text-center">两个我推.png</p>
       </div>
     </>
